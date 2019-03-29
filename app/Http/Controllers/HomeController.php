ffed2cd914f5eac0d751961c11ef6423425411ec
sravya <saisravyam@gmail.com>
2019-03-29 00:47:40 -0400

    Initial Set Up

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+    public function profile()
+    {
+
+        $user=User::find(2);
+        $user->delete();
+        $users=User::all();
+
+        return view('profile',['users' => $users]);
+    }
+}
